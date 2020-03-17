da76f0a2cd3a1bab23f181eaaad190ea7319499a
Genna Mergola <gennamergola@gmail.com>
2020-03-16 17:28:18 -0700

    first 10

---
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,22 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    
+    let_freq = {}
+    for letter in phrase:
+        if not letter in let_freq:
+            let_freq[letter] = 1
+        else:
+            let_freq[letter]+= 1
+
+    return let_freq
+
+
+print(multiple_letter_count('yay'))
+print(multiple_letter_count('Yay'))
